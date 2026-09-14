49c1ae89fddd19e41e9553fd5e2c93cfe2b9575e
NEYLON EDUARDO <neyloneduardo@yahoo.com>
2026-09-14 20:03:14 -0300

    refactor: delete unnecessary lines

--- src/main/java/com/ney/taskmanager/infrastructure/HTTP/GlobalExceptionHandler.java
+++ src/main/java/com/ney/taskmanager/infrastructure/HTTP/GlobalExceptionHandler.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.View;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    public GlobalExceptionHandler(View error) {
-    }
 
     @ExceptionHandler(TaskNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
